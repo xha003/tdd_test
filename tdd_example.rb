bc34335a9898a148c0f6cf257141eecdac1f6ec8
xhagrg <grg.iksha@hotmail.com>
2015-01-07 22:19:05 +0545

    Include let/before, after examples.

--- tdd_example.rb
+++ tdd_example.rb
@@ -1,8 +1,20 @@
 require './reverser.rb'
 
 describe 'passed values should be reversed' do
+  let!(:test) { print 'hello world abc.    ' }  
+  let(:another_test) { print 'Hi! I am another test.   ' }
+
+  before do 
+    print "hello world 123  "
+  end
+
+  after do
+    print "Bye World  "
+  end
+
   describe '.reverse_string' do
     it 'reverses the string passed' do
+      test
       reversed_string = Reverser.reverse_string('hello world')
       expect(reversed_string).to eql('dlrow olleh')
     end
