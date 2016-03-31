3c86ea9c4d0e8b06778914100fdc1e79e0c7ac93
bhishmaCF <bhishma@cloudfactory.com>
2016-03-31 11:39:24 +0545

    Check repeat complete.

--- codes/reverser.rb
+++ codes/reverser.rb
@@ -3,9 +3,7 @@ class Reverser
     def reverse_string(string_to_reverse)
       string_to_reverse.reverse
     end
-  end
-
-  class << self
+  
     def reverse_array(array_to_reverse)
       array_to_reverse.reverse
     end
