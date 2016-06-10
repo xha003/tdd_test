22b0aae06deae13215d5223b28c1c7d738f18659
bibhutiregmi <bibhutiregmi@gmail.com>
2016-06-10 15:20:01 +0545

    Corrected TDD

--- codes/reverser.rb
+++ codes/reverser.rb
@@ -3,5 +3,14 @@ class Reverser
     def reverse_string(string_to_reverse)
       string_to_reverse.reverse
     end
+
+    def reverse_array(array_to_reverse)
+      array_to_reverse.reverse
+    end
+
+    def reverse(array_to_reverse)
+      array_to_reverse.reverse
+    end
+
   end
 end
